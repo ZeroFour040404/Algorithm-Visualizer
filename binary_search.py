0c60bc2cac0bb9822d3456f3445fa3c892d9f9c5
Krizzmyr Ezzekiel Acierto <krizzmyrezzekielacierto@gmail.com>
2026-01-06 16:54:22 +0800

    design and quality of life

--- binary_search.py
+++ binary_search.py
@@ -3,12 +3,15 @@ from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt, QTimer
 import math
 import sys
+import tkinter
 
 class BinarySearchApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
         self.clock = QTimer(self)
+        self.timer1 = QTimer(self)
+        self.timer2 = QTimer(self)
         self.clock.timeout.connect(self.continue_search)
         self.count = 0
 
@@ -75,7 +78,7 @@ class BinarySearchApp(QMainWindow):
                                   """)
         
         self.line = QLabel('', self)
-        self.line.setGeometry(300, 245, 10, 60)
+        self.line.setGeometry(300, 245, 0, 60)
         self.line.setStyleSheet("""
                                 background-color: yellow;
                                 """)
@@ -94,6 +97,21 @@ class BinarySearchApp(QMainWindow):
                                     font-weight: bold;
                                     """)
         
+        self.range_low = QLabel('a', self)
+        self.range_low.setFont(QFont('Arial', 10)) 
+        self.range_low.setGeometry(50, 200, 0, 50)
+        self.range_low.setStyleSheet("""
+                                     color: red;
+                                     """)   
+        
+        self.range_high = QLabel('a', self)
+        self.range_high.setFont(QFont('Arial', 10)) 
+        self.range_high.setGeometry(540, 200, 0, 50)
+        self.range_high.setStyleSheet("""
+                                     color: red;
+                                     """)   
+        
+            
     def start_search(self):
         self.box.setText('')
         self.count = 0
@@ -105,40 +123,66 @@ class BinarySearchApp(QMainWindow):
         self.last = len(self.data) - 1
         self.lower_bound.setText(str(self.data[0]))
         self.upper_bound.setText(str(self.data[len(self.data) - 1]))
+        self.block1.setGeometry(50, 250, 0, 50)
+        self.block2.setGeometry(50, 250, 0, 50)
+        self.line.setGeometry(300, 245, 0, 60)
+        self.range_high.setText('')
+        self.range_low.setText('')
+        self.guess.setText('')
                     
         self.clock.start(1000)
 
     def continue_search(self):
         # Placeholder for binary search logic
         self.count += 1
-        mid = math.ceil((self.first + self.last) / 2)
+        self.mid = math.floor((self.first + self.last) / 2)
         if self.first > self.last:
             self.box.setText("Failed")
-            return -1
-        
-        print(f"Checking middle value: {self.data[mid]}")
-        if self.data[mid] == self.value:
-            self.line.setGeometry(50 + int(mid * 490 / (self.data[-1] - self.data[0])), 245 , 10, 60)
-            self.guess.setGeometry(50 + int(mid * 490 / (self.data[-1] - self.data[0])), 300 , 50, 50)
-            self.guess.setText(str(self.data[mid]))
-            self.attempts.setText('Number of Attempts: ' + str(self.count))
-            self.box.setText(f"Success | Found: {self.data[mid]}")
+            self.attempts.setText(self.attempts.text() + ' | Failed')
             self.clock.stop()
             return
-        elif self.data[mid] > self.value:
-            self.last = mid - 1
+        
+        print(f"Checking self.middle value: {self.data[self.mid]}")
+        if self.data[self.mid] == self.value:
+            self.line.setGeometry(50 + int(self.mid * 490 / (self.data[-1] - self.data[0])), 245 , 10, 60)
+            self.guess.setGeometry(50 + int(self.mid * 490 / (self.data[-1] - self.data[0])), 300 , 50, 50)
+            self.guess.setText(str(self.data[self.mid]))
+            self.attempts.setText('Number of Attempts: ' + str(self.count))
+            self.attempts.setText(f"{self.attempts.text()} | Success | Found: {self.data[self.mid]}")
+            self.clock.stop()
+            return
+        elif self.data[self.mid] > self.value:
+            self.last = self.mid - 1
             print('self.last',self.last)
-            self.block2.setGeometry(50 + int(mid * 500 / (self.data[-1] - self.data[0])), 250, int(500 - (int(mid * 500 / (self.data[-1] - self.data[0])))), 50)
+            self.timer2.start(990)
+            self.timer2.timeout.connect(self.set_block2)
         else:
-             self.first = mid + 1
+             self.first = self.mid + 1
              print('self.first',self.first)
+             self.timer1.start(990)
+             self.timer1.timeout.connect(self.set_block1)
              
-        self.line.setGeometry(50 + int(mid * 490 / (self.data[-1] - self.data[0])), 245 , 10, 60)
-        self.guess.setGeometry(50 + int(mid * 490 / (self.data[-1] - self.data[0])), 300 , 50, 50)
-        self.guess.setText(str(self.data[mid]))
+        self.line.setGeometry(50 + int(self.mid * 490 / (self.data[-1] - self.data[0])), 245 , 10, 60)
+        self.guess.setGeometry(50 + int(self.mid * 490 / (self.data[-1] - self.data[0])), 300 , 50, 50)
+        self.guess.setText(str(self.data[self.mid]))
             
         self.attempts.setText('Number of Attempts: ' + str(self.count))
         
+        
+    def set_block1(self):
+        self.block1.setGeometry(50, 250, int(self.mid * 500 / (self.data[-1] - self.data[0])), 50)
+        self.range_low.setGeometry(50 + int(self.mid * 500 / (self.data[-1] - self.data[0])), 200, 50, 50)
+        self.range_low.setText(str(self.data[self.mid]))
+        self.timer1.stop()
+        
+    
+    def set_block2(self):
+        self.block2.setGeometry(50 + int(self.mid * 500 / (self.data[-1] - self.data[0])), 250, int(500 - (int(self.mid * 500 / (self.data[-1] - self.data[0])))), 50)
+        self.range_high.setGeometry(50 + int(self.mid * 500 / (self.data[-1] - self.data[0])), 200, 50, 50)
+        self.range_high.setText(str(self.data[self.mid]))
+        self.timer2.stop()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
